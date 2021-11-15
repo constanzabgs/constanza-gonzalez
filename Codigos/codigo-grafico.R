206b8c3f43144d13756b1f08a11e81dc7d27a97c
constanzabgs <cbgonzalez8@uc.cl>
2021-11-15 12:42:14 -0300

    correcion errores grafico

--- Codigos/codigo-grafico.R
+++ Codigos/codigo-grafico.R
@@ -4,8 +4,9 @@ datos_2011 = rio::import(ruta) #base 2011
 library(tidyverse)
 
 ggplot(datos_2011) +
-  aes(x = Causas, weight = Defunciones, colour=Defunciones) +
+  aes(x = Causas, weight = Defunciones) +
   geom_bar(fill = "#C77979") +
+  scale_y_continuous(limits = c(0, 15), breaks = seq(0, 15, by =1))+
   labs(
     x = "Causas",
     y = "Cantidad de Defunciones",
@@ -13,14 +14,17 @@ ggplot(datos_2011) +
     subtitle = "Chile 2011"
   ) +
   coord_flip() +
-  theme_light()
+  theme_light()+
+  theme(text = element_text(size=8))
+
+
 
 ruta2 = file.choose()
 datos_2012 = rio::import(ruta2) #base 2012
 
 
 ggplot(datos_2012) +
-  aes(x = Causas, weight = Defunciones, colour=Defunciones) +
+  aes(x = Causas, weight = Defunciones) +
   geom_bar(fill = "darkorchid1") +
   labs(
     x = "Causas",
@@ -29,14 +33,15 @@ ggplot(datos_2012) +
     subtitle = "Chile 2012"
   ) +
   coord_flip() +
-  theme_light()
+  theme_light()+
+  theme(text = element_text(size=8))
 
 ruta3 = file.choose()
 datos_2013 = rio::import(ruta3) #base 2013
 
 
 ggplot(datos_2013) +
-  aes(x = Causas, weight = Defunciones, colour=Defunciones) +
+  aes(x = Causas, weight = Defunciones) +
   geom_bar(fill = "gray58") +
   labs(
     x = "Causas",
@@ -45,14 +50,15 @@ ggplot(datos_2013) +
     subtitle = "Chile 2013"
   ) +
   coord_flip() +
-  theme_light()
+  theme_light()+
+  theme(text = element_text(size=8))
 
 ruta4 = file.choose()
 datos_2014 = rio::import(ruta4) #base 2014
 
 
 ggplot(datos_2014) +
-  aes(x = Causas, weight = Defunciones, colour=Defunciones) +
+  aes(x = Causas, weight = Defunciones) +
   geom_bar(fill = "lightsalmon2") +
   labs(
     x = "Causas",
@@ -61,4 +67,5 @@ ggplot(datos_2014) +
     subtitle = "Chile 2014"
   ) +
   coord_flip() +
-  theme_light()
+  theme_light()+
+  theme(text = element_text(size=8))
